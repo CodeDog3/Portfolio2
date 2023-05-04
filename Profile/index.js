2a0c98da2418a6e1bc669e21ad21990cc1305a40
Jmbrenn <89528880+Jmbrenn@users.noreply.github.com>
2023-05-03 21:52:18 -0400

    moving box

--- Profile/index.js
+++ Profile/index.js
@@ -2,12 +2,27 @@ const backward = document.querySelectorAll('.movebck');
 const forward = document.querySelectorAll('.movefwd');
 const spinner = document.querySelector('.image-cube');
 
+let clicked = false;
 let rotatedeg = 0;
+
+
+const rb = () =>{
+  if(!clicked){
+    rotatedeg -= 90;
+    spinner.style.transform = `rotateY(${rotatedeg}deg)`;
+  }
+
+}
+
+forward.forEach((button) => {
+  button.addEventListener('click', rb);
+});
 
 forward.forEach((button) => {
   button.addEventListener('click', () => {
     rotatedeg += 90;
     spinner.style.transform = `rotateY(${rotatedeg}deg)`;
+    clicked = true;
   });
 });
 
@@ -15,5 +30,8 @@ backward.forEach((button) => {
     button.addEventListener('click', () => {
       rotatedeg -= 90;
       spinner.style.transform = `rotateY(${rotatedeg}deg)`;
+      clicked = true;
     });
   });
+
+  setInterval(rb,5000);
